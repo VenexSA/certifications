be0ecb812e9fe60ac5551e44eae987a999184fcb
mari0ar <mario.artaz@venex.com.ar>
2025-10-07 12:59:44 -0300

    debugging de los filtros

--- js/main.js
+++ js/main.js
@@ -9,13 +9,11 @@ async function loadCertifications() {
   try {
     const res = await fetch("https://certifications-backend-jnnv.onrender.com/api/getCertificates");
     const data = await res.json();
-    console.log(data)
     renderTable(data);
   } catch (err) {
     console.error("Error cargando certificados:", err);
   }
 }
-
 
 
 
